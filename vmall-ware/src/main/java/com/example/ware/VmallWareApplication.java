7a7018e0e9ae52fa04d13562305299fba18911dc
smokghos <linforest0067@gmail.com>
2025-09-18 17:01:09 +0800

    初始项目配置

---
+++ vmall-ware/src/main/java/com/example/ware/VmallWareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmallWareApplication.class, args);
+    }
+
+}
